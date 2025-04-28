6de19f794b6c3ab3c7b61393efebe9f6b0d4b8f8
Manuel <manuelalberto.gorrin@gmail.com>
2025-04-28 00:45:38 -0400

    fix: add update packages

--- utils/updateSquidStats.py
+++ utils/updateSquidStats.py
@@ -2,16 +2,8 @@ import json
 import subprocess
 import platform
 
-def update_squid():
+def updateSquidStats():
     try:
-        os_info = platform.freedesktop_os_release()
-        os_id = os_info.get('ID', '').lower()
-        codename = os_info.get('VERSION_CODENAME', os_info.get('UBUNTU_CODENAME', '')).lower()
-
-        if os_id not in ['ubuntu', 'debian'] or not codename:
-            print('Sistema operativo no compatible', 'error')
-            return False
-
         version_response = subprocess.run(
             ['curl', '-s', 'https://api.github.com/repos/cuza/squid/releases/latest'],
             capture_output=True, text=True
@@ -25,26 +17,6 @@ def update_squid():
             latest_version = json.loads(version_response.stdout)['tag_name']
         except (json.JSONDecodeError, KeyError):
             print('Error procesando versión', 'error')
-            return False
-
-        # Create name package
-        package_name = f"squid_{latest_version}-{os_id}-{codename}_amd64.deb"
-        download_url = f"https://github.com/cuza/squid/releases/download/{latest_version}/{package_name}"
-
-        check_package = subprocess.run(
-            ['wget', '--spider', download_url],
-            capture_output=True, text=True
-        )
-
-        if check_package.returncode != 0:
-            print(f'Paquete no disponible para {os_id.capitalize()} {codename.capitalize()}', 'error')
-            return False
-
-        download = subprocess.run(['wget', download_url, '-O', f'/tmp/{package_name}'])
-        subprocess.run(['apt', 'update'])
-
-        if download.returncode != 0:
-            print('Error descargando el paquete', 'error')
             return False
 
         install = subprocess.run(['dpkg', '-i', '--force-overwrite', f'/tmp/{package_name}'])
